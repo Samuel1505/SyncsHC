56f921a0609f0d721ece85f4dbed1ec2228cca12
Samuel1505 <oluwakoredeakinola@gmail.com>
2026-01-29 18:36:28 +0100

    update

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,8 +2,13 @@
 
 import Navigation from './components/Navigation';
 import Hero from './components/Hero';
+import Stats from './components/Stats';
 import Features from './components/Features';
+import Benefits from './components/Benefits';
 import HowItWorks from './components/HowItWorks';
+import UseCases from './components/UseCases';
+import Security from './components/Security';
+import FAQ from './components/FAQ';
 import CTA from './components/CTA';
 import Footer from './components/Footer';
 import PageTransition from './components/PageTransition';
@@ -11,11 +16,16 @@ import PageTransition from './components/PageTransition';
 export default function Home() {
   return (
     <PageTransition>
-      <main className="min-h-screen bg-white dark:bg-black w-full overflow-x-hidden">
+      <main className="min-h-screen bg-black w-full overflow-x-hidden">
         <Navigation />
         <Hero />
+        <Stats />
         <Features />
+        <Benefits />
         <HowItWorks />
+        <UseCases />
+        <Security />
+        <FAQ />
         <CTA />
         <Footer />
       </main>
